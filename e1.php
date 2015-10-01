801875135c10635f311bd07437bfd43207d6e848
hmoircbao <h.moircabo@gmail.com>
2015-10-02 00:37:32 +0100

    primer php commit

---
+++ e1.php
@@ -0,0 +1,10 @@
+<?php 
+
+	$random = mt_rand(100,200)
+?>
+	<html>
+		<head><title>e1</title></head>
+		<body>
+		<h3><b><u>Numero aleatorio entre 100 y 200, ambos inclusive</u></b></h3>
+		<h1> <?php echo $random ?>
+	</html>
