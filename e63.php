37f160dc9a4a116e91b3b72c34b37a76639e4cf8
hmoircbao <h.moircabo@gmail.com>
2015-10-02 01:00:35 +0100

    update de e1 y e63

--- e63.php
+++ e63.php
@@ -1,6 +1,10 @@
+<?php 
+
+	$random = mt_rand(100,200)
+?>
 	<html>
 		<head><title>e1</title></head>
 		<body>
 		<h3><b><u>Numero aleatorio entre 100 y 200, ambos inclusive</u></b></h3>
-		<h1> <?php echo mt_rand(100,200) ?></h1>
+		<h1> <?php echo $random ?></h1>
 	</html>
